48d1399901b088180cfb887af006ea704dc1fb79
Daniil <danik.frenk@gmail.com>
2019-06-27 21:13:06 +0300

    'book'

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/books','BookController@index')->name('list_book');
+Route::get('/book','BookController@create')->name('create');
+Route::post('/book','BookController@store')->name('create_book');
+
+
+Route::get('/book/{id}','BookController@show')->name('show_book');
+Route::delete('/book/{id}','BookController@destroy');
